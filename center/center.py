bce6cc8a5696851a4f4b474402dbf3a8c7986318
wenke.lai <wenke.studio@gmail.com>
2024-07-25 17:20:33 +0800

    feat: add a class name for debug

--- center/center.py
+++ center/center.py
@@ -3,6 +3,12 @@ import reflex as rx
 from center import pages  # noqa
 from center.features.api import service
 
-app = rx.App()
+app = rx.App(
+    style={
+        ".debug": {
+            "border": "1px solid red",
+        }
+    }
+)
 
 app.api.include_router(service)
